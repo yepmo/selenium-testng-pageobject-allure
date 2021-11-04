d8e4c6d7a9b3fecec99c6c67ef7862102e85cd9f
ParthibanRajasekaran <rajasekaran.parthiban7@gmail.com>
2021-11-04 16:34:59 +0100

    create group to skip workflow execution

--- src/main/java/com/template/project/common/Groups.java
+++ src/main/java/com/template/project/common/Groups.java
@@ -3,6 +3,7 @@ package com.template.project.common;
 public class Groups {
 
   public static final String AUTH = "auth";
+  public static final String NOT_FOR_REMOTE_EXECUTION = "not_for_remote_execution";
 
   public static final String INPUT_FIELD = "input_field";
 }
